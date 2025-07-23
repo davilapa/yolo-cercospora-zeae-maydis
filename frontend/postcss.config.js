134ea01b23ac58c720fdb5b7d836582904810d1e
davilapa <diegoavilapava@gmail.com>
2025-07-22 22:15:05 -0500

    fix: tailwindcss problems

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import tailwindcss from '@tailwindcss/vite'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
